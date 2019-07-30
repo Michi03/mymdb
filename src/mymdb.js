e588e576b2bc5c55365dbb1c4d28e2201f763fdb
Michael Kretschmer <michael.kretschmer@rwth-aachen.de>
2019-07-30 02:20:46 +0200

    ratings now properly update

--- src/mymdb.js
+++ src/mymdb.js
@@ -1,59 +1,120 @@
+var directorName = "";
+var title = "";
+var id = "";
+var rating = "0";
+var storedRating = "0";
+var dirDiv = {};
+var dirMovies;
+
 (function() {
     if (window.hasRun === true)
         return;
     window.hasRun = true;
-    var dirDiv = document.createElement("div");
+    dirDiv = document.createElement("div");
     dirDiv.id = "mymdb";
     dirDiv.setAttribute("class", "credit_summary_item");
     dirDiv.innerHTML = '<h4 class="inline">Other movies:</h4>';
     document.querySelector(".plot_summary").appendChild(dirDiv);
-    // get director
-    var director = document.querySelector(".credit_summary_item").children[1].innerHTML;
-    // add other movies
-    var getRatings = browser.storage.local.get(director, function(res) {
-        alert("The director object:" + JSON.stringify(res.key));
-    });
-
+    // get movies of director
+    directorName = document.querySelector(".credit_summary_item").children[1].innerHTML;
+    browser.storage.sync.get(directorName, gotDir);
 })();
 
-function addRating(id,title,director) {
-
-    browser.storage.local.set({key: directors[key]});
+function updateRating() {
+    rating = document.querySelector(".star-rating-value").innerHTML;
+    if (storedRating !== rating && document.querySelector("#nbusername") !== null)
+    {
+        storedRating = rating;
+        console.log("Change detected!");
+        if (rating === "0")
+        {
+            // delte id from director movie array
+            dirMovies.forEach(function(movie,idx) {
+                if (movie === id)
+                {
+                    delete(dirMovies[idx]);
+                    console.log(dirMovies);
+                    var store = {};
+                    store[directorName] = dirMovies;
+                    browser.storage.sync.set(store, onSet);
+                }
+            });
+        }
+        else
+        {
+            var store = {};
+            store[id] = [title,rating];
+            browser.storage.sync.set(store, onSet);
+            if (typeof dirMovies === "undefined")
+            {
+                // add movie to rated movies
+                dirMovies = [id]
+                store = {};
+                store[directorName] = dirMovies;
+                browser.storage.sync.set(store, onSet);
+            }
+            else
+            {
+                dirMovies.forEach(function(movie,idx) {
+                    if (movie === id)
+                        return;
+                });
+                // add movie to rated movies
+                dirMovies.push(id);
+                store = {};
+                store[directorName] = dirMovies;
+                browser.storage.sync.set(store, onSet);
+            }
+        }
+    }
 }
 
-function onGot(director) {
-    console.log("SUCCESS");
-    console.log(director);
+function gotDir(directorObj) {
     // get current movie
-    var url = window.location.href;
-    var id = url.split('/')[4];
-    var title = document.querySelector("h1").firstChild.data;
-    var rating = document.querySelector(".star-rating-value").innerHTML;
+    id = window.location.href.split('/')[4];
+    title = document.querySelector("h1").firstChild.data;
+    rating = document.querySelector(".star-rating-value").innerHTML;
     var dirDiv = document.querySelector("#mymdb");
     var inStorage = false;
-    for (var i = 0; i < director.length; i++) {
-        var movie = director[i];
-        if (movie.id === id)
-            continue;
-        if (dirDiv.children.length > 1)
-            dirDiv.innerHTML += ", ";
-        var movieLink = document.createElement("a");
-        movieLink.setAttribute("href", "/title/" + movie.id);
-        movieLink.innerHTML = movie.title + " (" + movie.rating + ")";
-        dirDiv.appendChild(movieLink);
-    }
-    // if movie is rated store rating
-    if (inStorage === false)
+    dirMovies = directorObj[directorName];
+    if (typeof dirMovies !== "undefined")
     {
-        if (rating === 0)
-            return;
-        director.push({"id":id, "title": title, "rating": rating});
-        directorName = document.querySelector(".credit_summary_item").children[1].innerHTML;
-        browser.storage.local.set({directorName: director});
+        console.log(dirMovies);
+        // get other movies of director
+        dirMovies.forEach(function(movie) {
+            // movie = {id:[title,rating]}
+            browser.storage.sync.get(movie, gotMovie);
+        });
+    }
+    // periodically check if rating changed
+    setInterval(updateRating, 1000);
+}
+
+function gotMovie(movieObj) {
+    var curId = Object.keys(movieObj)[0];
+    var movie = movieObj[curId];
+    if (typeof movie !== "undefined" && typeof movie[1] !== "undefined")
+    {
+        if (curId === id)
+            storedRating = movie[1];
+        else
+        {
+            if (dirDiv.children.length > 1)
+                dirDiv.innerHTML += ", ";
+            var movieLink = document.createElement("a");
+            movieLink.setAttribute("href", "/title/" + curId);
+            movieLink.innerHTML = movie[0] + " (" + movie[1]+ ")";
+            dirDiv.appendChild(movieLink);
+        }
     }
 }
 
 function onErr(val) {
     console.log("ERROR");
+    console.log(val);
+}
+
+function onSet(val) {
+    console.log("STORED");
     console.log(val);
 }
---
+++ src/ratings.js
@@ -0,0 +1,67 @@
+const directors = [];
+
+function readRatings(e) {
+    e.preventDefault();
+    var input = document.querySelector("#file").files[0];
+    var reader = new FileReader();
+    reader.onload = function(evt) {
+        parseRatings(reader.result);
+    }
+    reader.readAsText(input);
+}
+
+function parseRatings(dataString) {
+    var lines = dataString.split(/\r\n|\n/);
+    document.querySelector("#output").innerHTML = "";
+    for (var i = 1; i < lines.length; i++)
+    {
+        var fields = lines[i].split(',');
+        // store id, title and rating
+        var movie = {"id": fields[0], "title": fields[3], "rating": fields[1]};
+        // get director
+        var curDir = fields[fields.length-1];
+        if (curDir[curDir.length-1] === "\"")
+            curDir = curDir.substr(0,curDir.length-1);
+        if (typeof directors[curDir] === "undefined")
+            directors[curDir] = [movie];
+        else
+            directors[curDir].push(movie);
+        // store director with movie array
+        var item = document.createElement("li");
+        item.innerHTML = curDir + ", " + JSON.stringify(movie);
+        document.querySelector("#output").appendChild(item);
+    }
+}
+
+function storeRatings(e) {
+    e.preventDefault();
+    var items = Object.keys(directors);;
+    document.querySelector("#output").innerHTML = "";
+    items.forEach(function(dir) {
+        var dirMovies = [];
+        directors[dir].forEach(function(movie) {
+            dirMovies.push(movie.id);
+            var store = {};
+            store[movie.id] = [movie.title,movie.rating];
+            browser.storage.sync.set(store, outputRating);
+        });
+        // MOST INFURIATING CRAP EVER!
+        var store = {};
+        store[dir] = dirMovies;
+        browser.storage.sync.set(store, outputRating);
+    });
+}
+
+function getStorage() {
+    browser.storage.sync.get(null, outputRating);
+}
+
+function outputRating(elem) {
+    var output = document.createElement("li");
+    output.innerHTML = JSON.stringify(elem);
+    document.querySelector("#output").appendChild(output);
+}
+
+document.addEventListener("DOMContentLoaded", getStorage);
+document.querySelector("#file").addEventListener("change", readRatings, false);
+document.querySelector("#submit").addEventListener("click", storeRatings, false);
