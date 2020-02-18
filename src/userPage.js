c7d9212b5a7fe7bf6f1846b6af06d89fd7296b07
Michi <mic.michael@gmx.de>
2020-02-18 15:18:10 +0100

    added star filter

--- src/userPage.js
+++ src/userPage.js
@@ -1,8 +1,8 @@
 // make all the necessary changes
-document.head.innerHTML ='<meta charset="utf-8"><title>MyMDB User Page</title><link rel="icon" type="image/png" href="https://raw.githubusercontent.com/Michi03/mymdb/master/src/icon.png" /><style>body{font-family:Gothic,sans-serif;background-color:#CCC;}header{background-color:#111;display:flex;width:100%;justify-content:space-between;}a{text-decoration:none;}.inline{display:inline;}.container{display.flex;justify-content:center;width:75%;margin-left:12.5%;background-color:#FFF;}#mymdbIcon{font-family:Arial,Helvetica,sans-serif;display:inline-block;padding:0.2em;background-color:#E4CD17;color:#000;margin:25%1em;border-radius:10px;}#headerText{color:#E4CD17;}#search{border-radius:20px;width:10%;display:inline-block;margin:1.5em;}</style>';
+document.head.innerHTML ='<meta charset="utf-8"><title>MyMDB User Page</title><link rel="icon" type="image/png" href="https://raw.githubusercontent.com/Michi03/mymdb/master/src/icon.png" /><style>body{font-family:Gothic,sans-serif;background-color:#CCC;}header{background-color:#111;display:flex;width:100%;justify-content:space-between;}a{text-decoration:none;}.inline{display:inline;}.container{display.flex;justify-content:center;width:75%;margin-left:12.5%;background-color:#FFF;}#mymdbIcon{font-family:Arial,Helvetica,sans-serif;display:inline-block;padding:0.2em;background-color:#E4CD17;color:#000;margin:25%1em;border-radius:10px;}#headerText{color:#E4CD17;}#search{border-radius:20px;width:10%;display:inline-block;margin:1.5em;}#stars{color:#FFF;cursor:pointer;}</style>';
 document.body.removeChild(document.body.children[0]);
 let header = document.createElement("header");
-header.innerHTML = '<a href="https://imdb.com/"><div id="mymdbIcon"><h2 class="inline">MyMDB</h2></div></a><h1 class="inline" id="headerText">Your MyMDB Ratings</h1><input type="text" id="search" placeholder="Filter"></div>';
+header.innerHTML = '<a href="https://imdb.com/"><div id="mymdbIcon"><h2 class="inline">MyMDB</h2></div></a><h1 class="inline" id="headerText">Your MyMDB Ratings</h1><div id="stars"><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1></div><input type="text" id="search" placeholder="Filter Directors">';
 document.body.appendChild(header);
 let container = document.createElement("div");
 container.classList.add("container");
@@ -12,6 +12,7 @@ const list = document.querySelector("#dirList");
 browser.storage.sync.get("username", gotUsername);
 document.querySelector("#search").addEventListener("change", filter, false);
 reset();
+addStars();
 
 function gotUsername(data){
     // change heading and title if username is known
@@ -50,12 +51,10 @@ function gotMovies(data) {
                 let row = document.createElement("tr");
                 row.innerHTML = "<td>" + dir["name"] + "</td><td></td>";
                 list.appendChild(row);
-                let first = true;
                 dir["movies"].forEach(function(movie) {
-                    if (!first)
-                        row.children[1].innerHTML += ",";
-                    first = false;
-                    row.children[1].innerHTML += " <a href='https://www.imdb.com/title/" + movie + "'>" + movies[movie].title + " (" + movies[movie].rating + ")</a>";
+                    let item = document.createElement("td");
+                    item.innerHTML += " <a href='https://www.imdb.com/title/" + movie + "'>" + movies[movie].title + " (" + movies[movie].rating + ")</a>";
+                    row.appendChild(item);
                 });
             }
         });
@@ -66,48 +65,92 @@ function filter() {
     let pattern = document.querySelector("#search").value;
     if (pattern.length < 1)
         return;
-    let result = "";
+    let deleteRow = [];
     for (let n = 0; n < list.children.length; n++)
     {
         let row = list.children[n];
         let match = 0;
-        let str = row.innerHTML;
-        let insideHtml = true;
+        let str = row.children[0].innerText;
         for (let i = 0; i < str.length; i++)
         {
-            if (str[i] === '<')
-            {
-                insideHtml = true;
-                continue;
-            }
-            if (str[i] === '>')
-            {
-                insideHtml = false;
-                continue;
-            }
-            if (insideHtml)
-                continue;
             if (str[i].toLowerCase() === pattern[match].toLowerCase())
                 match++;
             else
                 match = 0;
             if (match >= pattern.length)
-            {
-                result += "<tr>" + str + "</tr>";
                 break;
-            }
         }
+        if (match < pattern.length)
+            deleteRow.push(row);
     }
+    for (let i = 0; i < deleteRow.length; i++)
+        list.removeChild(deleteRow[i]);
+    addReset();
+}
+
+function addReset() {
     let rstBtn = document.createElement("button");
     rstBtn.setAttribute("id", "resetBtn");
     rstBtn.innerHTML = "Reset Filter";
-    list.innerHTML = result;
     list.appendChild(rstBtn);
     rstBtn.addEventListener("click", reset);
 }
 
+function addStars() {
+    let stars = document.querySelector("#stars").children;
+    for (let i = 0; i < stars.length; i++)
+    {
+        stars[i].setAttribute("id",i+1);
+        stars[i].addEventListener("click", filterStars, false);
+    }
+}
+
+function filterStars(e) {
+    let rating = e.target.id;
+    let stars = document.querySelector("#stars").children;
+    let deleteRow = [];
+    for (let i = 0; i < rating; i++)
+        stars[i].innerHTML = "&#9733;";
+    let pattern = "(" + rating + ")";
+    if (pattern.length < 1)
+        return;
+    for (let n = 0; n < list.children.length; n++)
+    {
+        let row = list.children[n];
+        let deleteItem = [];
+        for (let j = 1; j < row.children.length; j++)
+        {
+            let match = 0;
+            let str = row.children[j].innerText;
+            for (let i = 0; i < str.length; i++)
+            {
+                if (str[i] === pattern[match])
+                    match++;
+                else
+                    match = 0;
+                if (match >= pattern.length)
+                    break;
+            }
+            if (match < pattern.length)
+                deleteItem.push(row.children[j]);
+        }
+        if (deleteItem.length >= row.children.length - 1)
+            deleteRow.push(row);
+        else
+            for (let j = 0; j < deleteItem.length; j++)
+                row.removeChild(deleteItem[j]);
+    }
+    for (let i = 0; i < deleteRow.length; i++)
+        list.removeChild(deleteRow[i]);
+    addReset();
+}
+
 function reset() {
+    list.innerHTML = "";
     browser.storage.sync.get(null, gotMovies);
     document.querySelector("#search").value = "";
-    list.removeChild(document.querySelector("#resetBtn"));
+    let stars = document.querySelector("#stars").children;
+    for (let i = 0; i < stars.length; i++)
+        stars[i].innerHTML = "&#9734;";
+
 }
