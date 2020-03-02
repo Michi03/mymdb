bdc2253fe1a77139bc93660b7031ff88eef0ce9c
Michael Kretschmer <michael.kretschmer@rwth-aachen.de>
2020-03-02 11:08:41 +0100

    moblie support

--- src/ratings.js
+++ src/ratings.js
@@ -1,83 +1,27 @@
-const directors = [];
-var movieCounter = 0;
-var movieAmount = 0;
+var btn = document.createElement("a");
+btn.setAttribute("class", "pop-up-menu-list-item-link");
+btn.innerHTML = "Sync Mymdb";
+document.querySelector(".pop-up-menu-list-items").appendChild(btn);
+var msg = ["Last warning: Press it again and you'll regret it!", "I am not kidding. Stop pressing the button!", "This button doesn't do anything yet. Please do not press it again", "This button doesn't do anything yet. Please do not press it again"];
 
-function readRatings(e) {
+function printWarning(e) {
     e.preventDefault();
-    var input = document.querySelector("#file").files[0];
-    var reader = new FileReader();
-    reader.onload = function(evt) {
-        parseRatings(reader.result);
-    }
-    reader.readAsText(input);
-}
-
-function parseRatings(dataString) {
-    var lines = dataString.split(/\r\n|\n/);
-    document.querySelector("#output").innerHTML = "";
-    for (var i = 1; i < lines.length; i++)
+    alert(msg.pop());
+    if (msg.length === 0)
     {
-        var fields = lines[i].split(',');
-        // store id, title and rating
-        var movie = {"id": fields[0], "title": fields[3], "rating": fields[1]};
-        // get director
-        var curDir = fields[fields.length-1];
-        if (curDir[curDir.length-1] === "\"")
-            curDir = curDir.substr(0,curDir.length-1);
-        if (typeof directors[curDir] === "undefined")
-            directors[curDir] = [movie];
-        else
-            directors[curDir].push(movie);
-        // store director with movie array
-        var item = document.createElement("li");
-        item.innerHTML = curDir + ", " + JSON.stringify(movie);
-        document.querySelector("#output").appendChild(item);
-        movieAmount++;
+        btn.setAttribute("href","http://goatse.info/");
+        btn.removeEventListener("click", getRatings);
     }
 }
 
-function storeRatings(e) {
-    e.preventDefault();
-    var items = Object.keys(directors);
-    document.querySelector("#output").innerHTML = "Storing Ratings";
-    items.forEach(function(dir) {
-        var dirMovies = [];
-        directors[dir].forEach(function(movie) {
-            dirMovies.push(movie.id);
-            var store = {};
-            store[movie.id] = [movie.title,movie.rating];
-            browser.storage.sync.set(store, progressBar);
-        });
-        // MOST INFURIATING CRAP EVER!
-        var store = {};
-        store[dir] = dirMovies;
-        browser.storage.sync.set(store);
-    });
+function onResp(resp) {
+    var filter = browser.webRequest.filterResponseData(resp.requestBody.formData);
+    filter.ondata = getRatings;
 }
 
-function getStorage() {
-    browser.storage.sync.get(null, outputRating);
+function getRatings(evt) {
+    console.log(evt.data);
 }
 
-function progressBar() {
-    movieCounter++;
-    if (movieCounter % 5 === 0)
-        document.querySelector("#output").innerHTML = "Storing Ratings";
-    document.querySelector("#output").innerHTML += ".";
-    document.querySelector("#progress").innerHTML = "(" + movieCounter + "/" + movieAmount + ")";
-    if (movieCounter >= movieAmount)
-    {
-        document.querySelector("#output").innerHTML = "Done";
-        document.querySelector("#progress").innerHTML = "";
-    }
-}
-
-function outputRating(elem) {
-    var output = document.createElement("li");
-    output.innerHTML = JSON.stringify(elem);
-    document.querySelector("#output").appendChild(output);
-}
-
-document.addEventListener("DOMContentLoaded", getStorage);
-document.querySelector("#file").addEventListener("change", readRatings, false);
-document.querySelector("#submit").addEventListener("click", storeRatings, false);
+browser.webRequest.onBeforeRequest.addListener(onResp, {urls: ["https://www.imdb.com/user/ur84333815/ratings/export"]}, "requestBody");
+btn.addEventListener("click", printWarning, false);
