c391549a0bac03218e977f33e6275471f08fac74
Michi <mic.michael@gmx.de>
2020-03-09 00:20:17 +0100

    implemented burger Menu and other stuff for userPage

--- src/userPage.js
+++ src/userPage.js
@@ -1,8 +1,8 @@
 // make all the necessary changes
-document.head.innerHTML ='<meta charset="utf-8"><title>MyMDb User Page</title><link rel="icon" type="image/png" href="https://mymdb.org/icons/mymdb.png" /><style>body{font-family:Gothic,sans-serif;background-color:#CCC;}header{background-color:#111;display:flex;width:100%;justify-content:space-between;}a{text-decoration:none;}li{padding:.25em;display:block;}.dir{margin:0 1.5em;cursor:pointer;}.inline{display:inline;}.container{display.flex;justify-content:center;width:75%;margin-left:12.5%;background-color:#FFF;}#mymdbIcon{font-family:Arial,Helvetica,sans-serif;display:inline-block;padding:0.2em;background-color:#E4CD17;color:#000;margin:25% 1em;border-radius:10px;cursor:pointer;}#headerText{color:#E4CD17;}#search{border-radius:20px;display:inline-block;margin:1.5em;}#stars{color:#FFF;cursor:pointer;}#filterDiv{width:10%}#imdbLink{padding:0;color:#E4CD17;font-family:Gothic,sans-serif;}</style>';
+document.head.innerHTML ='<meta charset="utf-8"><title>MyMDb User Page</title><link rel="icon" type="image/png" href="https://mymdb.org/icons/mymdb.png" /><meta name="viewport" content="width=device-width, initial-scale=1.0"><style>body{font-family:Gothic,sans-serif;background-color:#CCC;}hr{margin:0;padding:0}header{background-color:#111;display:flex;width:100%;justify-content:space-between;}a{text-decoration:none;}li{display:block;padding:.15em 0 0 .15em}h3{margin:0;padding:.5em .2em;}ul{margin:0;padding:0 0 .5em 1.5em;transition:max-height .2s;overflow:hidden;}.dir{cursor:pointer;transition:.4s;padding:1em;background-color:#fff;}.dir:hover{background-color:#F8FA7E;padding-left:2em}.inline{display:inline;}.active{padding-left:2em;}.container{width:75%;margin-left:12.5%;background-color:#FFF;}.appears{display:none;}.dropdown{width:200px;}#dropdown{position:absolute;z-index:5;}#mymdbIcon{font-family:Arial,Helvetica,sans-serif;display:inline-block;padding:0.2em;background-color:#E4CD17;color:#000;margin:25% 1em;border-radius:10px;cursor:pointer;}#headerText{color:#E4CD17;margin-top:35px}#search{border-radius:20px;display:inline-block;margin:1.5em;}#stars{color:#FFF;cursor:pointer;}#filterDiv{width:10%}#burgerMenu{cursor:pointer;padding:0 1em;}#imdbLink{padding:0;color:#E4CD17;font-family:Gothic,sans-serif;}@media screen and (max-width: 870px){.disappears{display:none;}.appears{display:inline-block;}#headerText{margin-top:10px;}header{display:block;}}</style>';
 document.body.removeChild(document.body.children[0]);
-let header = document.createElement("header");
-header.innerHTML = '<div><a href="https://mymdb.org/" target="_blank"><div id="mymdbIcon"><h2 class="inline">MyMDb</h2></div></a><a href="/list/ratings" target="_blank"><h2 id="imdbLink" class="inline">IMDB</h2></a></div><h1 class="inline" id="headerText">Your MyMDb Ratings</h1><div><input type="text" id="search" placeholder="Filter Directors"><div id="stars"><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1></div></div>';
+const header = document.createElement("header");
+header.innerHTML = '<div id="burgerMenu" class="appears"><img height=30px width=40px src="https://mymdb.org/imgs/burger.png" /><div id="dropdown" style="display:none;"><div class="dir dropdown" style="transition:0s;">Expand All</div></div></div><div class="disappears"><a href="https://mymdb.org/" target="_blank"><div id="mymdbIcon"><h2 class="inline">MyMDb</h2></div></a><a href="/list/ratings" target="_blank"><h2 id="imdbLink" class="inline">IMDB</h2></a></div><h1 class="inline" id="headerText">Your MyMDb Ratings</h1><div class="disappears"><input type="text" id="search" placeholder="Filter Directors"><div id="stars"><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1><h1 class="inline">&#9734;</h1></div><div id="btnDiv"></div></div>';
 document.body.appendChild(header);
 const list = document.createElement("div");
 list.classList.add("container");
@@ -11,6 +11,10 @@ browser.storage.sync.get("username", gotUsername);
 document.querySelector("#search").addEventListener("change", filter, false);
 reset();
 addStars();
+var expand = true;
+document.querySelector("#burgerMenu").addEventListener("click", openDropdown, false);
+document.querySelector("#dropdown").children[0].addEventListener("click", toggleExpanded, false);
+
 
 function gotUsername(data){
     // change heading and title if username is known
@@ -29,6 +33,16 @@ function gotMovies(data) {
     }
     else
     {
+        if (!document.querySelector('#expBtn'))
+        {
+            let expBtn = document.createElement("div");
+            expBtn.setAttribute("id", "expBtn");
+            expBtn.innerHTML = "Expand All";
+            expBtn.setAttribute('style','display:inline-block;padding:0;width:50%;');
+            document.querySelector('#btnDiv').appendChild(expBtn);
+            expBtn.classList.add('dir');
+            expBtn.addEventListener("click", toggleExpanded);
+        }
         let keys = Object.keys(data);
         let directors = [];
         let movies = {};
@@ -57,8 +71,8 @@ function gotMovies(data) {
                 dirItem.innerHTML = "<h3>" + dir["name"] + "</h3>";
                 list.appendChild(dirItem);
                 let dirList = document.createElement('ul');
-                dirList.setAttribute('hidden',true);
                 dirItem.appendChild(dirList);
+                dirList.style.maxHeight = '0px';
                 dir["movies"].forEach(function(movie) {
                     if (typeof movies[movie] !== 'undefined')
                     {
@@ -114,11 +128,12 @@ function filter() {
 function addReset() {
     if (document.querySelector("#resetBtn") !== null)
         return;
-    let rstBtn = document.createElement("button");
+    let rstBtn = document.createElement("div");
     rstBtn.setAttribute("id", "resetBtn");
     rstBtn.innerHTML = "Reset Filter";
-    list.insertBefore(document.createElement('hr'), list.firstChild);
-    list.insertBefore(rstBtn, list.firstChild);
+    rstBtn.setAttribute('style','display:inline-block;padding:0;width:50%;');
+    document.querySelector('#btnDiv').appendChild(rstBtn);
+    rstBtn.classList.add('dir');
     rstBtn.addEventListener("click", reset);
 }
 
@@ -174,6 +189,8 @@ function filterStars(e) {
     }
     for (let i = 0; i < deleteRow.length; i++)
         deleteRow[i].parentElement.removeChild(deleteRow[i]);
+    expand = true;
+    toggleExpanded();
     addReset();
 }
 
@@ -187,16 +204,65 @@ function reset() {
     let stars = document.querySelector("#stars").children;
     for (let i = 0; i < stars.length; i++)
         stars[i].innerHTML = "&#9734;";
+    if (document.querySelector('#expBtn'))
+    {
+        expand = false;
+        toggleExpanded();
+    }
 }
 
 function toggleHidden(e) {
     let hiddenElement = e.target;
     while (hiddenElement.tagName !== 'DIV')
         hiddenElement = hiddenElement.parentElement;
+    hiddenElement.classList.toggle('active');
     hiddenElement = hiddenElement.children[1];
-    hiddenElement.hidden = !hiddenElement.hidden;
+    if (hiddenElement.style.maxHeight === '0px')
+    {
+        hiddenElement.style.maxHeight = hiddenElement.scrollHeight + 'px';
+    }
+    else
+    {
+        hiddenElement.style.maxHeight = '0px';
+    }
 }
 
+function toggleExpanded() {
+    let lists = document.querySelectorAll('ul');
+    lists.forEach(function(elem) {
+        if (!expand)
+        {
+            elem.style.maxHeight = '0px';
+        }
+        else
+        {
+            elem.style.maxHeight = elem.scrollHeight + 'px';
+        }
+    });
+    if (expand)
+    {
+        document.querySelector('#expBtn').innerHTML = 'Collapse All';
+        document.querySelector('#dropdown').children[0].innerHTML = 'Collapse All';
+    }
+    else
+    {
+        document.querySelector('#expBtn').innerHTML = 'Expand All';
+        document.querySelector('#dropdown').children[0].innerHTML = 'Expand All';
+    }
+    expand = !expand;
+}
+
+function openDropdown(e) {
+    e.stopPropagation();
+    document.querySelector("#dropdown").style.display = 'block';
+    document.body.addEventListener("click", closeDropdown, false);
+}
+
+function closeDropdown(e) {
+    e.stopPropagation();
+    document.querySelector("#dropdown").style.display = 'none';
+    document.body.removeEventListener("click", closeDropdown);
+}
 
 //    Copyright (c) 2019-2020
 //    This file is part of The MyMDb Firefox Web-Extension.
